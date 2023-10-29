b1e8aafa1c753afa167b2cab619e9a8cd163972a
Jens de Ruiter <jensgamesnl@gmail.com>
2023-10-29 15:14:02 +0100

    Add slow falling effect

--- src/main/java/dev/jensderuiter/balloonsbyjens/util/Utils.java
+++ src/main/java/dev/jensderuiter/balloonsbyjens/util/Utils.java
@@ -6,6 +6,8 @@ import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class Utils {
@@ -24,6 +26,7 @@ public class Utils {
                 }
                 if (item.getType() == Material.PLAYER_HEAD && item.getItemMeta().getLore() != null && item.getItemMeta().getLore().contains("Balloon")) {
                     BalloonRunner balloonRunner = new BalloonRunner(player, item);
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, -1, 0, false));
                     balloonRunner.runTaskTimer(BalloonsPlugin.getInstance(), 0, 1);
                     BalloonsPlugin.playerBalloons.put(player, balloonRunner);
                 }
@@ -36,6 +39,7 @@ public class Utils {
             runner.moveLocation.getWorld().spawnParticle(Particle.CLOUD, runner.moveLocation, 5, 0, 0 ,0, 0.1);
             runner.destroy();
             runner.cancel();
+            runner.player.removePotionEffect(PotionEffectType.SLOW_FALLING);
             BalloonsPlugin.playerBalloons.remove(runner.player);
         }
     }
